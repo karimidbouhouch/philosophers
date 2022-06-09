bac6b944fb6f0b098a75043ef3a57d350cfd1e4b
karimidbouhouch <karim.idbouhouch@gmail.com>
2022-06-09 10:23:45 +0100

    bonus update

--- philo_bonus/philo_bonus.c
+++ philo_bonus/philo_bonus.c
@@ -12,79 +12,135 @@
 
 #include "philo_bonus.h"
 
-int	make_thread(t_all *info, int j, int i)
-{
-	if (info->philo[i].philo_id % 2 == j)
-	{
-		if (pthread_create(&info->philo[i].thread, NULL,
-				&routine, &info->philo[i]) != 0)
-		{
-			destroy_mutex(info);
-			free(info->lock);
-			free(info->lock->forks);
-			return (1);
-		}
-	}
-	usleep(30);
-	return (0);
-}
 
-int	if_dead(t_philo *philo)
-{
-	long	time;
-
-	time = current_time() - philo->last_meal;
-	if (time > philo->args->time_to_die)
-	{
-		pthread_mutex_lock(&philo->mutex->output);
-		printf("%ld\tThe philo %d is dead\n",
-			current_time() - philo->args->start_time, philo->philo_id);
-		philo->args->dead = 1;
-		return (1);
-	}
-	return (0);
-}
-
-int	check_death(t_all *info)
+void	wait_loop(t_all *all)
 {
 	int	i;
-	int	count_meals;
+	int	j;
+	int	status;
 
+	i = 0;
+	while (i < all->data->nb_of_philo)
+	{
+		waitpid(-1, &status, 0);
+		if (status == 0)
+		{
+			j = 0;
+			while (j < all->data->nb_of_philo)
+			{
+				kill(all->philo->pid[j], SIGTERM);
+				j++;
+			}
+		}
+		i++;
+	}
+}
+
+void	*check_death(void *philos)
+{
+	t_philo		*phil;
+	int			meals_count;
+	long		hunger_time;
+
+	phil = (t_philo *)philos;
 	while (1)
 	{
-		i = 0;
-		count_meals = 0;
-		while (i < info->data->nb_of_philo)
+		meals_count = 0;
+		hunger_time = get_time() - phil->last_meal;
+		if (phil->args->time_to_die < hunger_time)
 		{
-			if (if_dead(&info->philo[i]))
-				return (1);
-			count_meals += info->philo[i].eat_count;
-			i++;
+			sem_wait(phil->sems->output);
+			printf("%ld\tThe philo %d is dead\n", get_time() - \
+			phil->start_time, phil->philo_id);
+			exit (0);
 		}
-		if (count_meals == 0)
+		meals_count += phil->eat_count;
+		if (meals_count == 0)
 		{
-			pthread_mutex_lock(&info->lock->output);
-			return (1);
+			sem_wait(phil->sems->output);
+			exit (0);
 		}
 	}
+}
+
+void	is_sleep(long wait_time)
+{
+	long	starting_time;
+	long	time_passed;
+
+	starting_time = get_time();
+	time_passed = get_time() + wait_time;
+	while (time_passed > starting_time)
+	{
+		usleep(100);
+		starting_time = get_time();
+	}
+}
+
+void	output(t_philo *philo, char *str)
+{
+	sem_wait(philo->sems->output);
+	if (philo->args->death_flag == 0)
+		printf("%ld\tThe philo %d %s\n", get_time() - philo->start_time, \
+		philo->philo_id, str);
+	sem_post(philo->sems->output);
+}
+
+int	eating(t_philo *philo)
+{
+	sem_wait(philo->sems->forks);
+	output(philo, "has taken the forks");
+	output(philo, "is eating");
+	philo->last_meal = get_time();
+	is_sleep(philo->args->time_to_eat);
+	sem_post(philo->sems->forks);
 	return (0);
 }
 
-int	ft_threads(t_all *info)
+void	start_act(t_philo *phil)
+{
+	pthread_t	dead_flag;
+
+	phil->start_time = get_time();
+	phil->last_meal = get_time();
+	pthread_create(&dead_flag, NULL, check_death, (void *)phil);
+	pthread_detach(dead_flag);
+	while (1)
+	{
+		if (phil->eat_count != 0)
+		{
+			eating(phil);
+			phil->eat_count--;
+			output(phil, "is sleeping");
+			is_sleep(phil->args->time_to_sleep);
+			output(phil, "is thinking");
+		}
+		else
+			exit (0);
+	}
+}
+
+int	init_process(t_all *all)
 {
 	int	i;
 
-	i = -1;
-	info->data->start_time = current_time();
-	while (++i < info->data->nb_of_philo)
-		make_thread(info, 0, i);
-	i = -1;
-	while (++i < info->data->nb_of_philo)
-		make_thread(info, 1, i);
-	if (check_death(info))
-		return (0);
+	i = 0;
+	while (i < all->data->nb_of_philo)
+	{
+		all->philo->pid[i] = fork();
+		if (all->philo->pid[i] == 0)
+		{
+			all->philo->philo_id = i + 1;
+			start_act(all->philo);
+		}
+		else if (all->philo->pid[i] == -1)
+			return (put_err("Can't create a process\n", 0));
+		i++;
+	}
+	wait_loop(all);
 	return (0);
 }
+
 
 int	main(int ac, char **av)
 {
@@ -104,7 +160,7 @@ int	main(int ac, char **av)
 	info.data->dead = 0;
 	if (av[5] != NULL)
 		info.data->nb_of_meals = ft_atoi(av[5]);
-	if (init_mutex(&info))
+	if (init_sems(&info))
 		return (0);
 	if (init_philo(&info))
 		return (0);
--- philo_bonus/philo_bonus.h
+++ philo_bonus/philo_bonus.h
@@ -18,6 +18,7 @@
 # include <stdlib.h>
 # include <semaphore.h>
 # include <sys/time.h>
+# include <sys/stat.h>
 
 typedef struct sema
 {
